2e40b1dcc18912cfe99ab0146d9ec349ade19af5
Stefan Kanev <stefan.kanev@gmail.com>
2024-10-13 11:53:32 +0300

    Added basic authentication
    Should come back to this and clean it up, but the streaming gods are not
    in favor of doing it right now.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,5 +12,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#index"
 end
