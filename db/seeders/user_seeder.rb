e0f5310210c81d39944196784921fed001e814ba
Stefan Kanev <stefan.kanev@gmail.com>
2024-10-20 21:33:17 +0300

    Dress up the seeds
    - introduces a nicer DSL for creating users and their stuff
    
    - add a semi-real recipe in the seeds (blame Copilot for the "semi-"
      part)

---
+++ db/seeders/user_seeder.rb
@@ -0,0 +1,47 @@
+class UserSeeder
+  class << self
+    def define(&block)
+      DSL.new.instance_eval(&block)
+    end
+  end
+
+  class DSL
+    def user(**attributes, &block)
+      seeder = UserSeeder.new(**attributes)
+      seeder.instance_eval(&block) if block_given?
+    end
+  end
+
+  def initialize(**attributes)
+    @user = User.create!(attributes)
+    @products = {}
+  end
+
+  def products(text)
+    text.lines.each do |line|
+      /^(?<name>.+): (?<calories>\d+)kcal \/ (?<units_per_kcal>\d+)(?<unit>.+)$/ =~ line or raise "Invalid line: #{line}"
+      @products[name] = Product.create! name:, calories:, user: @user, units_per_kcal:, unit:
+    end
+  end
+
+  def recipe(name, text)
+    recipe = Recipe.new name:, user: @user
+
+    text.split(/^# /).reject(&:empty?).each do |chunk|
+      section, body = chunk.split("\n", 2)
+
+      case section
+      in 'Ingredients:'
+        body.lines.each do |line|
+          next if line.strip.empty?
+          /^(?<name>.+): (?<quantity>\d+)(?<unit>.+)?$/ =~ line or raise "Invalid line: #{line}"
+          recipe.ingredients.build product: @products.fetch(name), quantity: quantity
+        end
+      in 'Instructions:'
+        recipe.instructions = body
+      end
+    end
+
+    recipe.save!
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,42 @@
-user = User.create! email_address: 'user@example.com', password: '1'
+require_relative 'seeders/user_seeder'
 
-onion  = Product.create! name: 'Onion', calories: 60, user: user, units_per_kcal: 100, unit: 'gr'
-tomato = Product.create! name: 'Tomato', calories: 40, user: user, units_per_kcal: 100, unit: 'gr'
-egg    = Product.create! name: 'Egg', calories: 80, user: user, units_per_kcal: 1, unit: 'egg'
-butter = Product.create! name: 'Butter', calories: 750, user: user, units_per_kcal: 100, unit: 'gr'
+UserSeeder.define do
+  user email_address: 'user@example.com', password: '1' do
+    products <<~END
+      onion: 60kcal / 100gr
+      mince meat: 332kcal / 100gr
+      potatoes: 93kcal / 100gr
+      yogurt: 59kcal / 100gr
+      tomato: 40kcal / 100gr
+      egg: 80kcal / 1egg
+      butter: 750kcal / 100gr
+      kashkaval: 400kcal / 100gr
+      flour: 350kcal / 100gr
+      vegatable oil: 900kcal / 100gr
+    END
 
-slop = Recipe.create! name: 'Egg slop', user: user
-slop.ingredients.create! product: onion, quantity: 200
-slop.ingredients.create! product: tomato, quantity: 100
-slop.ingredients.create! product: egg, quantity: 2
+    recipe 'Moussaka', <<~END
+      # Ingredients:
 
-omlette = Recipe.create! name: 'Omlette', user: user
-omlette.ingredients.create! product: egg, quantity: 2
-omlette.ingredients.create! product: butter, quantity: 40
+      mince meat: 500gr
+      onion: 200gr
+      potatoes: 1000gr
+      tomato: 400gr
+      yogurt: 400gr
+      egg: 2
+      flour: 50gr
+      vegatable oil: 40gr
 
-another = User.create! email_address: 'another@example.com', password: '1'
+      # Instructions:
 
-Product.create! name: 'Cucumber', calories: 20, user: another, units_per_kcal: 100, unit: 'gr'
+      1. Cut the onion and fry it in the oil.
+      2. Add the mince meat and fry it.
+      3. Add the potatoes and fry them.
+      4. Add the tomato and fry it.
+      5. Add the yogurt and fry it.
+      END
+  end
+
+  user email_address: 'another@example.com', password: '1'
+end
+
